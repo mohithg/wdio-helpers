15b7a09aac233b9ec92e8d0525c02a4ada29594d
Mohith G <mohithgm@gmail.com>
2020-09-05 15:02:35 +0530

    Fix test

--- test/test.js
+++ test/test.js
@@ -14,13 +14,12 @@ describe ('Google', () => {
         click('.slidingNav');
         clickWithText('a', 'API');
         waitForTextToAppear("h1", "API Docs");
-        waitForElementToGo('h1');
+        waitForElementToGo('h1', 0);
         setValue('input', 0, "something");
         browser.pause(3000);
         clickWithText('a', 'API');
         waitForUrl("https://webdriver.io/docs/api.html");
-
-        browser.pause(10000);
+        browser.pause(3000);
         // setValue('.gLFyf.gsfi', 0, 'wdio-helpers');
     })
 })
