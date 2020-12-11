bc6dce5f0bfea09f3f8e117b40047048628010ab
Mohith G <mohithgm@gmail.com>
2020-12-11 22:31:05 +0530

    Typescript module (#10)
    make everything a typescript module

--- src/helpers.ts
+++ src/helpers.ts
@@ -1,11 +1,17 @@
-import _ from 'lodash';
+import * as _ from 'lodash';
+
+declare global {
+  var browser: any;
+}
+
 /**
  * @function load
  * @desc loads the url in the browser.
  * @param {string} url - The url to navigate to.
+ * @param useBrowser - Optional browser instance to use, otherwise defaults to global browser
  */
-export const load = (url) => {
-  browser.url(url);
+export const load = (url: string, useBrowser: any = browser) => {
+  useBrowser.url(url);
 }
 
 /**
@@ -19,16 +25,18 @@ export const load = (url) => {
 * @param {String} url - URL to check if loaded
 * @param {Number} timeout - waiting time for url to load 
 * @param {regex} regex - Regex to match with url
+* @param useBrowser - Optional browser instance to use, otherwise defaults to global browser
 */
-export const waitForUrl = (url, timeout, regex = false) => {
-  browser.waitUntil(() => {
+export const waitForUrl = (url: string, timeout: number, regex: boolean = false, useBrowser: any = browser) => {
+  useBrowser.waitUntil(() => {
     if (regex) {
-      return url.test(browser.getUrl());
+      // @ts-ignore
+      return url.test(useBrowser.getUrl());
     }
-    return browser.getUrl() === url;
+    return useBrowser.getUrl() === url;
   },
   timeout,
-  `***Expected ${url} instead of ${browser.getUrl()}***`
+  `***Expected ${url} instead of ${useBrowser.getUrl()}***`
   );
 };
 
@@ -36,9 +44,10 @@ export const waitForUrl = (url, timeout, regex = false) => {
  * @function wait
  * @desc Accepts selector to wait for the element to appear in the DOM and scroll the page to that element in the screen.
  * @param {string} selector - The Selector element.
+ * @param useBrowser - Optional browser instance to use, otherwise defaults to global browser
  */
-export const wait = (selector) => {
-  const element = $(selector);
+export const wait = (selector: string, useBrowser: any = browser) => {
+  const element = useBrowser.$(selector);
   element.waitForExist({});
   element.moveTo({});
 }
@@ -49,10 +58,11 @@ export const wait = (selector) => {
  * Note that it waits until selector appears in the DOM. And clicks only the visible element.
  * @param {string} selector - The Selector element.
  * @param {number} [index = 0] - index number
+ * @param useBrowser - Optional browser instance to use, otherwise defaults to global browser
  */
-export const click = (selector, index = 0) => {
-  wait(selector);
-  const element = $$(selector)[index];
+export const click = (selector: string, index: number = 0, useBrowser: any = browser) => {
+  wait(selector, useBrowser);
+  const element = useBrowser.$$(selector)[index];
   if (element.isDisplayedInViewport()) {
     element.click();
   } else {
@@ -65,12 +75,13 @@ export const click = (selector, index = 0) => {
  * @desc Accepts selector to wait for the element to appear in the DOM and scroll the page to that element in the screen.
  * and waits until the selector is visible
  * @param {string} selector - The Selector element.
+ * @param useBrowser - Optional browser instance to use, otherwise defaults to global browser
  */
-export const waitForVisible = (selector) => {
+export const waitForVisible = (selector, useBrowser = browser) => {
   wait(selector);
-  browser.waitUntil(function() {
-    return $(selector).isDisplayedInViewport();
-  }, 100000, 'Selector is not visible');
+  useBrowser.waitUntil(function() {
+    return useBrowser.$(selector).isDisplayedInViewport();
+  }, 100000, `Selector ${selector} is not visible`);
 };
 
 /**
@@ -79,11 +90,12 @@ export const waitForVisible = (selector) => {
  * and waits until the selector with text is visible
  * @param {string} selector - The Selector element.
  * @param {string} textToSearch - Text to search in the selectors.
+ * @param useBrowser - Optional browser instance to use, otherwise defaults to global browser
  */
-export const waitForTextToAppear = (selector, textToSearch) => {
-  wait(selector);
-  const group = $$(selector);
-  browser.waitUntil(function() {
+export const waitForTextToAppear = (selector: string, textToSearch: string, useBrowser: any = browser) => {
+  wait(selector, useBrowser);
+  const group = useBrowser.$$(selector);
+  useBrowser.waitUntil(function() {
     for (let i = 0; i < group.length; i++) {
       const element = group[i];
       if (element.getText().includes(textToSearch)) {
@@ -99,9 +111,10 @@ export const waitForTextToAppear = (selector, textToSearch) => {
  * @param {string} selector - Selector for the element.
  * @param {string} text - The text element.
  * @param {number} [index = 0] - index number
+ * @param useBrowser - Optional browser instance to use, otherwise defaults to global browser
  */
-export const clickWithText = (selector, text, index = 0) => {
-  const element = findElementFromGroupWithText(selector, text, index);
+export const clickWithText = (selector: string, text: string, index: number = 0, useBrowser: any = browser) => {
+  const element = findElementFromGroupWithText(selector, text, index, useBrowser);
   if (element) {
     element.click();
   } else {
@@ -115,11 +128,12 @@ export const clickWithText = (selector, text, index = 0) => {
  * @param {string} selector - The Selector element.
  * @param {number} [index = 0] - index number
  * @param {string} [value = ''] - Value to be set in the selector
+ * @param useBrowser - Optional browser instance to use, otherwise defaults to global browser
  */
-export const setValue = (selector, index = 0, value = '') => {
-  wait(selector);
-  $$(selector)[index].clearValue();
-  $$(selector)[index].setValue(value);
+export const setValue = (selector: string, index: number = 0, value: string = '', useBrowser: any = browser) => {
+  wait(selector, useBrowser);
+  useBrowser.$$(selector)[index].clearValue();
+  useBrowser.$$(selector)[index].setValue(value);
 }
 
 /**
@@ -127,11 +141,12 @@ export const setValue = (selector, index = 0, value = '') => {
  * @desc Waits until element is not visible in DOM. Selector will choose based on the index.
  * @param {string} selector - The Selector element.
  * @param {number} [index = 0] - index number
+ * @param useBrowser - Optional browser instance to use, otherwise defaults to global browser
  */
-export const waitForElementToGo = (selector, index = 0) => {
-  browser.waitUntil(() => {
-    if (!_.isEmpty($$(selector))) {
-      const element = $$(selector)[index];
+export const waitForElementToGo = (selector: string, index: number = 0, useBrowser: any = browser) => {
+  useBrowser.waitUntil(() => {
+    if (!_.isEmpty(useBrowser.$$(selector))) {
+      const element = useBrowser.$$(selector)[index];
       return element && element.isDisplayedInViewport();
     } else {
         return true;
@@ -146,10 +161,11 @@ export const waitForElementToGo = (selector, index = 0) => {
  * @param {string} textToSearch - Enter the text to search in elements
  * @param {number} [index = 0] - index number
  * @return {element} element - The element which matches the textToSearch from group of groupSelector or returns null if nothing is found
+ * @param useBrowser - Optional browser instance to use, otherwise defaults to global browser
  */
-export const findElementFromGroupWithText = (groupSelector, textToSearch, index = 0) => {
-  wait(groupSelector);
-  const group = $$(groupSelector);
+export const findElementFromGroupWithText = (groupSelector: string, textToSearch: string, index: number = 0, useBrowser: any = browser) => {
+  wait(groupSelector, useBrowser);
+  const group = useBrowser.$$(groupSelector);
   let elements = [];
   for (let i = 0; i < group.length; i++) {
     const element = group[i];
@@ -172,7 +188,8 @@ const Helpers = {
   setValue,
   wait,
   waitForElementToGo,
-  waitForUrl
+  waitForUrl,
+  waitForVisible
 };
 
 export default Helpers;
